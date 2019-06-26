86380f734a54e8cc83be07766402d0af91afe331
Tamper Claire <tamperm1@t31300-lr017.org.aalto.fi>
2019-06-26 17:54:23 +0300

    Adding version

--- src/run.py
+++ src/run.py
@@ -68,13 +68,13 @@ def index():
 
         if code == 1:
             print('results',results)
-            data = {'status':200,'data':results, 'service':"Regex Identifier Service", 'date':dt.today().strftime('%Y-%m-%d')}
+            data = {'status':200,'data':results, 'service':"Regex Identifier Service", 'date':dt.today().strftime('%Y-%m-%d'), 'version':0.1}
             return jsonify(data)
         else:
-            data = {"status":-1,"Error":str(results), "service":"Regex Identifier Service", "date":dt.today().strftime('%Y-%m-%d')}
+            data = {"status":-1,"Error":str(results), "service":"Regex Identifier Service", "date":dt.today().strftime('%Y-%m-%d'), 'version':0.1}
             return jsonify(data)
     data = {"status": -1, "Error": "415 Unsupported Media Type ;)", "service": "Regex Identifier Service",
-            "date": dt.today().strftime('%Y-%m-%d')}
+            "date": dt.today().strftime('%Y-%m-%d'), 'version':0.1}
     return jsonify(data)
 
 
